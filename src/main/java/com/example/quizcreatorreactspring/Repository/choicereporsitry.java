4a71a6dca88fe19b4dadca428cb94e702e3bb6a7
kabeer <kabeer.cs17@iba-suk.edu.pk>
2021-06-12 21:38:57 +0500

    added choice

--- src/main/java/com/example/quizcreatorreactspring/Repository/choicereporsitry.java
+++ src/main/java/com/example/quizcreatorreactspring/Repository/choicereporsitry.java
@@ -4,5 +4,6 @@ import com.example.quizcreatorreactspring.Model.Choice;
 import org.springframework.data.repository.CrudRepository;
 
 public interface choicereporsitry extends CrudRepository<Choice,Integer> {
+
 }
 
